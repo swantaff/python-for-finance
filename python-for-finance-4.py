ac7ce760b8eb0f979bc717c71b1141fa5966fbd8
Mark Jones <mark.jones3@dvla.gov.uk>
2020-03-05 07:51:25 +0000

    Added working candlestick chart

--- python-for-finance-4.py
+++ python-for-finance-4.py
@@ -1,7 +1,6 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
-import mpl_finance
 from mpl_finance import candlestick_ohlc
 import matplotlib.dates as mdates
 import pandas as pd
@@ -9,52 +8,33 @@ import pandas_datareader.data as web
 
 style.use ('ggplot')
 
-#start = dt.datetime(2000,1,1)
-#end = dt.datetime(2020,3,3)
-
-#df = web.DataReader('TSLA', 'yahoo', start, end)
-
-#df.to_csv('tsla.csv')
 
 # read data from csv file
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
 
-print(df.tail()) # prints all columns
-
-print(df[['Open', 'High']].tail()) # print specified column along with index which is date
-
-#df['100ma'] = df['Adj Close'].rolling(window=100).mean()
-
+# resample data
 df_ohlc = df['Adj Close'].resample('10D').ohlc()
 df_volume = df['Volume'].resample('10D').sum()
 
-print(df.tail()) # prints all columns
+# candlestick chat requires mdates and ohlc columns
+# so we need to reset the data frame and include / convert these columns
+
+df_ohlc.reset_index(inplace=True) # adds index (row) number to dataframe
+
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num) # converts date to mdate format(think unix time buts its not)
 
 
-# lets remove rows where 100ma is NaN
-# df.dropna(inplace=True) 
-
-## or we can set min periods to zero so we a an average up to 100 max for the first
-# 100 values in the table
-#df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-
-#print(df.head()) # prints all columns
-
-#df.plot() # shows all columns
-#df['Adj Close'].plot() # shows just 'Adj Close' column
-#plt.show()
+print(df_ohlc.tail())
 
 
 #create graph template i.e. size etc.
 ax1 = plt.subplot2grid((6,1), (0,0,), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0,), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
 
-# specify x-y values for each line on the graphs above
-# x-values are the date fields which is df index
-# y-values are what columns we want to plot
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-# bar chart for volume
-ax2.bar(df.index, df['Volume'])
+# create candlestick graph
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+# show volume below
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 
 plt.show()
